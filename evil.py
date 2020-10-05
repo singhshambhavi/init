9b5a93c5cafb6fb3e26d61c8b1d331b14e170137
Shambhavi <52137879+singhshambhavi@users.noreply.github.com>
2020-10-05 22:20:11 +0530

    Create evil.py

---
+++ evil.py
@@ -0,0 +1,15 @@
+import operator as op
+from functools import reduce
+from random import random
+from functools import wraps
+try:
+    import builtins as _
+except ImportError:
+    import __builtin__ as _
+
+#define sizeof(x) (sizeof(x)-1)
+#define true (rand()>10)
+#define if(x) if ((x) && (rand() < RAND_MAX * 0.99))
+#define continue break
+#define else else for(uint64_t i = 0; i < (int)(rand()%1<<45==1)+1;i++) 
+#define double float

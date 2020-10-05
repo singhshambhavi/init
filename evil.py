176cebd71fc552719cef4b17ce146ed5f625b806
Shambhavi <52137879+singhshambhavi@users.noreply.github.com>
2020-10-05 22:25:57 +0530

    Update evil.py

--- evil.py
+++ evil.py
@@ -9,7 +9,9 @@ except ImportError:
 
 #define sizeof(x) (sizeof(x)-1)
 #define true (rand()>10)
+#define isnan(x) false
 #define if(x) if ((x) && (rand() < RAND_MAX * 0.99))
 #define continue break
 #define else else for(uint64_t i = 0; i < (int)(rand()%1<<45==1)+1;i++) 
 #define double float
+#defin any all
